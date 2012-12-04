560d535aefb9a785737cf12826d684a0d1128932
Stephane <stephane.dallongeville@pasteur.fr>
2012-12-04 11:36:24 +0100

    updated library to last kernel version.

--- updater/Main.java
+++ updater/Main.java
@@ -50,7 +50,7 @@ public class Main
     /**
      * Updater Version
      */
-    public static Version version = new Version("1.6.1.0");
+    public static Version version = new Version("1.6.2.0");
 
     static UpdateFrame frame = null;
     static String extraArgs = "";
